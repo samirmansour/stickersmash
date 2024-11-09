aeb20ce0e0d53e17de442018b79bb2ab2d2def6d
Samir Mansour <samtothemirror@gmail.com>
2024-11-08 20:43:27 -0600

    Added tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
     </View>
   );
 }
